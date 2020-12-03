e438c84b4ed698f4142ce0b51b234548e4c18312
Alex MacKenzie <alex-mack-code@gmail.com>
2020-12-03 01:52:57 -0500

    added first view and route

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-//require express
+//setup express
 const express = require("express");
 const app = express();
 
-//require server utilities
+//setup path
+var path = require("path");
+
+//setup handlebars
+const exphbs = require('express-handlebars');
+app.engine('.hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+//setup server utilities
 const myServerUtilities = require('./serverUtilities')
 
 // setup http server to listen on HTTP_PORT
 app.listen(myServerUtilities.getHttpPort(), myServerUtilities.onHttpStart());
+
+// route server to index
+app.get("/index", function(req,res)
+{
+    //setup data to pass to view
+    var someData = {
+        id: 1,
+        name: "Alex MacKenzie",
+        age: 32,
+        description: "software developer",
+    };
+    
+    res.render('viewData', {
+        data: someData,
+        layout: false // do not use the default Layout (main.hbs)
+    });
+        
+});
+
